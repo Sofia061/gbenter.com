ae480a47e03a2596c487524ae429802a8812b4b1
Sofia <golban.sofia2006@gmail.com>
2026-03-24 12:49:54 +0100

    commit7

--- js/js.js
+++ js/js.js
@@ -62,7 +62,6 @@ filterButtons.forEach((button) => {
   });
 });
 
-
 filterButtons.forEach((button) => {
   button.addEventListener("click", () => {
     document.querySelector(".filter-btn.active").classList.remove("active");
@@ -80,4 +79,24 @@ filterButtons.forEach((button) => {
       }
     });
   });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init("1LM3mNNHkRt3_n39I");
+
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      emailjs.sendForm("service_t694o9z", "template_93xyqmd", this).then(
+        function () {
+          alert("Message sent successfully!");
+        },
+        function (error) {
+          alert("Failed to send message.");
+          console.log(error);
+        },
+      );
+    });
 });
